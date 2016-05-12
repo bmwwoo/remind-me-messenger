5f50e00f4224ebaed0d5e2bfa7d518f26b455d5b
Brandon Woo <bmw.woo@gmail.com>
2016-05-11 20:05:36 -0700

    Modulus hello world working

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '<validation_token>') {
+  if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
-})
-
-app.get('/hello')
-
-app.post('/webhook', function (req, res) {
-  res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/hello', function (req, res) {
+  res.send('world');
 });
+
+app.listen(process.env.PORT);
