a86c88c330db2877d4591a5869e7408d357774c5
Brandon Woo <bmw.woo@gmail.com>
2016-05-11 20:19:53 -0700

    Facebook logging working

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+// verification
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
     res.send(req.query['hub.challenge']);
@@ -8,8 +12,23 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 });
 
+// receiving text
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      console.log(text);
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.get('/hello', function (req, res) {
   res.send('world');
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
