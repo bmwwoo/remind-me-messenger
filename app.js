1a55da3fa32dfb16025f55e96b00688d1001420a
Brandon Woo <bmw.woo@gmail.com>
2016-05-11 19:39:01 -0700

    Add app js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
+app.get('/hello')
+
+app.post('/webhook', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
